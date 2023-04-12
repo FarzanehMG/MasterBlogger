1d27dccdb04d68b93123cb2ccbf431c54f54948a
Farzaneh Gholian <farzaneh.golian2001@gmail.com>
2023-04-12 10:52:00 +0430

    Adding UI

---
+++ MB.Presentation.MVCCore/Pages/Index.cshtml.cs
@@ -0,0 +1,20 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace MB.Presentation.MVCCore.Pages
+{
+	public class IndeModel : PageModel
+	{
+		private readonly ILogger<IndeModel> _logger;
+
+		public IndeModel(ILogger<IndeModel> logger)
+		{
+			_logger = logger;
+		}
+
+		public void OnGet()
+		{
+
+		}
+	}
+}
