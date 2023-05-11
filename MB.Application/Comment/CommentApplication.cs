c24fba96a77b114a08a059d9d37d149e0bb76904
Farzaneh Gholian <farzaneh.golian2001@gmail.com>
2023-05-11 13:39:28 +0430

    Implemented Repository and Mapping Comment

--- MB.Application/Comment/CommentApplication.cs
+++ MB.Application/Comment/CommentApplication.cs
@@ -1,8 +1,15 @@
 ﻿using MB.Application.Contracts.Comment;
+using MB.Domain.CommentAgg;
 
 namespace MB.Application.Comment
 {
 	public class CommentApplication : ICommentApplication
 	{
+		private readonly ICommentRepository _comment;
+
+		public CommentApplication(ICommentRepository comment)
+		{
+			_comment = comment;
+		}
 	}
 }
--- MB.Infrastructure.EFCore/Mapping/ArticleMapping.cs
+++ MB.Infrastructure.EFCore/Mapping/ArticleMapping.cs
@@ -17,6 +17,7 @@ namespace MB.Infrastructure.EFCore.Mapping
 			builder.Property(x => x.IsDeleted);
 			builder.Property(x => x.CreationDate);
 			builder.HasOne(x => x.ArticleCategory).WithMany(x => x.Articles).HasForeignKey(x => x.ArticleCategoryId);
+			builder.HasMany(x=>x.Comments).WithOne(x=>x.Article).HasForeignKey(x => x.ArticleId);
 		}
 	}
 }
